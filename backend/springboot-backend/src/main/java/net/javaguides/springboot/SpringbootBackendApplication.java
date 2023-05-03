aa21a17d4a9d85c339bb637a237d76ffee1e3bcb
nathaliavi <67352231+nathaliavi@users.noreply.github.com>
2023-05-03 17:09:12 -0300

    Update SpringbootBackendApplication.java

--- backend/springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ backend/springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplicationAcn {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
